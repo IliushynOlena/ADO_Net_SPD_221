ca0b6902be88a782dc938e71bd8a80f63541a52d
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-09-16 17:47:24 +0300

    Entity Framework

---
+++ 06_IntroToEntityFramework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_IntroToEntityFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AirplaneDbContext context = new AirplaneDbContext();
+
+            context.Clients.Add(
+                new Client
+                {
+                    Name = "Volodia",
+                    Birthdate = new DateTime(2006, 7, 8),
+                    Email = "volodia@gmail.com"
+                });
+
+            context.SaveChanges();
+
+            foreach (var client in context.Clients)
+            {
+                Console.WriteLine( $"Client :  {client.Name}. {client.Email} " +
+                    $"{client.Birthdate.ToShortDateString()}");
+            }
+
+            //context.Airplanes.Select(i => i.Model);
+        }
+    }
+}
