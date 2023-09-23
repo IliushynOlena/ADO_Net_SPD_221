26254a2aeba3eeff6ff074bb451a9808bf7cf54d
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-09-23 17:28:35 +0300

    DataAnotations

--- 06_IntroToEntityFramework/AirplaneDbContext.cs
+++ 06_IntroToEntityFramework/AirplaneDbContext.cs
@@ -1,44 +1,65 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _06_IntroToEntityFramework
 {
-
+    [Table("Passangers")]
     //Entities
+
     public class Client
     {
+        //properties
+        //Primary key naming : Id/id/ID/ EntityName+Id(ClientId)
         public int Id { get; set; }
+        [Required]//null - not null
+        [MaxLength(100)]//nvarchar(max) - nvarchar(100)
+        [Column("FirstName")]//set column name
         public string Name { get; set; }
+        [Required, MaxLength(100)]
+        //[EmailAddress]
         public string Email { get; set; }
-        public DateTime Birthdate { get; set; }
+        //public Nullable< DateTime> Birthdate { get; set; }//not null- ? null
+        public DateTime ?Birthdate { get; set; }//not null- ? null
+
+        //Relationship type
         public ICollection<Flight> Flights { get; set; }
 
     }
     public class Airplane
     {
+        //properties
         public int Id { get; set; }
+        [Required, MaxLength(100)]
         public string Model { get; set; }
         public int MaxPassangers { get; set; }
+        //Relationship type
         public ICollection<Flight> Flights { get; set; }
     }
     public class Flight
     {
-        public int Id { get; set; }
+        //properties
+        //public int Id { get; set; }
+        //Primary key naming : Id/id/ID/ EntityName+Id(ClientId)
+        [Key]//Primary key
+        public int Number { get; set; }
         public DateTime DepartureTime { get; set; }
         public DateTime ArrivalTime { get; set; }
+        [Required, MaxLength(100)]
         public string DepartureCity { get; set; }
+        [Required, MaxLength(100)]
         public string ArrivalCity { get; set; }
-
+        //Relationship type
         //RelationshipType : One to many (1.....*)
         public Airplane Airplane { get; set; }//null
+        //ForeignKey naming : RelatedEntityName + RelatedEntityPrimaryKeyName
         public int AirplaneId { get; set; }
         public ICollection<Client> Clients { get; set; }
-
-
 
     }
     public class AirplaneDbContext : DbContext  
@@ -49,7 +70,8 @@ namespace _06_IntroToEntityFramework
         //Flights
         public AirplaneDbContext()
         {
-            this.Database.EnsureCreated();
+            //this.Database.EnsureDeleted();
+            //this.Database.EnsureCreated();
         }
         public DbSet<Client> Clients { get; set; }
         public DbSet<Airplane> Airplanes { get; set; }
@@ -64,6 +86,55 @@ namespace _06_IntroToEntityFramework
                                             Encrypt=False;TrustServerCertificate=False;
                                             ApplicationIntent=ReadWrite;
                                             MultiSubnetFailover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //Initialization
+            modelBuilder.Entity<Airplane>().HasData(new Airplane[]
+            {
+                new Airplane()
+                {
+                     Id = 1,
+                     Model = "Boing 747",
+                     MaxPassangers = 100                      
+                },
+                new Airplane()
+                {
+                     Id = 2,
+                     Model = "AN 125",
+                     MaxPassangers = 125
+                },
+                new Airplane()
+                {
+                     Id = 3,
+                     Model = "F16",
+                     MaxPassangers = 1
+                }
+            });
+
+
+            modelBuilder.Entity<Flight>().HasData(new Flight[]
+              {
+                new Flight()
+                {
+                     Number = 1,
+                     DepartureCity = "Rivne",
+                     ArrivalCity = "Lviv",
+                     ArrivalTime = new DateTime(2023,5,7,13,30,30),
+                     DepartureTime = new DateTime(2023,5,7,7,25,25),
+                     AirplaneId = 1
+                },
+                 new Flight()
+                {
+                     Number = 2,
+                     DepartureCity = "Warshav",
+                     ArrivalCity = "Lviv",
+                     ArrivalTime = new DateTime(2023,6,17,15,30,30),
+                     DepartureTime = new DateTime(2023,6,17,6,25,25),
+                     AirplaneId = 2
+                }
+              });
         }
 
     }
--- 06_IntroToEntityFramework/Program.cs
+++ 06_IntroToEntityFramework/Program.cs
@@ -9,23 +9,42 @@ namespace _06_IntroToEntityFramework
         {
             AirplaneDbContext context = new AirplaneDbContext();
 
-            context.Clients.Add(
-                new Client
-                {
-                    Name = "Volodia",
-                    Birthdate = new DateTime(2006, 7, 8),
-                    Email = "volodia@gmail.com"
-                });
+            //Linq to Entities
+            var filteredFlights = context.Flights
+                .Where(f => f.ArrivalCity == "Lviv")
+                .OrderBy(f => f.DepartureCity);
 
-            context.SaveChanges();
-
-            foreach (var client in context.Clients)
+            foreach (var f in filteredFlights)
             {
-                Console.WriteLine( $"Client :  {client.Name}. {client.Email} " +
-                    $"{client.Birthdate.ToShortDateString()}");
+                Console.WriteLine($"Flight :  {f.Number}. From {f.DepartureCity} " +
+                    $" to {f.ArrivalCity}");
             }
+
+
+            //context.Clients.Add(
+            //    new Client
+            //    {
+            //        Name = "Volodia",
+            //        Birthdate = new DateTime(2006, 7, 8),
+            //        Email = "volodia@gmail.com"
+            //    });
+
+            //context.SaveChanges();
+
+            //foreach (var client in context.Clients)
+            //{
+            //    Console.WriteLine( $"Client :  {client.Name}. {client.Email} " +
+            //        $"{client.Birthdate}");
+            //}
 
             //context.Airplanes.Select(i => i.Model);
         }
+
+        ////string name = null;
+        //Nullable<int> a = null;
+        //int ?b = null;
+        ////int b = 0;
+        //Client cl = null;
+        //Client ?cl1 = 0;
     }
 }
